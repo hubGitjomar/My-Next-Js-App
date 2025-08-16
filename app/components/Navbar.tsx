735d17c94186fa176dc2624d7254b69deb802405
hubGitjomar <actjomar@gmail.com>
2025-08-16 23:42:39 +0800

    addChanges1

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="flex border-amber-600 border-1 rounded-2xl p-2 justify-between ">
+      <Link href="../user">
+        <Image src="/favicon.ico" alt="dikc" width={32} height={32} />
+      </Link>
+      <ul className="flex gap-4">
+        <Link
+          className="hover:bg-[#483AA0] transition duration-300 p-1"
+          href="../user/projects"
+        >
+          Projects
+        </Link>
+        <Link
+          className="hover:bg-[#483AA0] transition duration-300 p-1"
+          href="../user/skills"
+        >
+          Skills/Services
+        </Link>
+        <Link
+          className="hover:bg-[#483AA0] transition duration-300 p-1"
+          href="../user/resume"
+        >
+          Resume
+        </Link>
+        <Link
+          className="hover:bg-[#483AA0] transition duration-300 p-1"
+          href="../user/contact"
+        >
+          Contact
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import Link from "next/link";
 import React from "react";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <Link
-        className="font-bold border rounded-b-lg transition duration-300 ease-in-out hover:bg-amber-400 p-2"
-        href="./pages"
-      >
-        Users
-      </Link>
+      <Navbar />
     </main>
   );
 }
